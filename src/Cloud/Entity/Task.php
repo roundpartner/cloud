c4ed095ceec1642b4877a161a5958dd61e2b34e1
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2016-04-08 17:07:54 +0100

    Adding values to task entity for tasks

--- src/Cloud/Entity/Task.php
+++ src/Cloud/Entity/Task.php
@@ -4,5 +4,23 @@ namespace RoundPartner\Cloud\Entity;
 
 class Task
 {
+    /**
+     * @var string
+     */
     public $taskName;
+
+    /**
+     * @var string
+     */
+    public $command;
+
+    /**
+     * @var string
+     */
+    public $action;
+
+    /**
+     * @var string[]
+     */
+    public $arguments;
 }
