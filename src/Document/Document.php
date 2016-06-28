9b5e6e4c43b91e4b73fb286867d1149e3d9fba56
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2016-06-28 10:47:14 +0100

    Add get container and checking if it exists

--- src/Document/Document.php
+++ src/Document/Document.php
@@ -2,6 +2,7 @@
 
 namespace RoundPartner\Cloud\Document;
 
+use Guzzle\Http\Exception\BadResponseException;
 use OpenCloud\Rackspace;
 
 class Document
@@ -27,5 +28,32 @@ class Document
     {
         $this->client = $client;
         $this->service = $this->client->objectStoreService($serviceName, $region);
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function containerExists($name)
+    {
+        return $this->getContainer($name) !== false;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return \OpenCloud\ObjectStore\Resource\Container
+     */
+    public function getContainer($name)
+    {
+        try {
+            return $this->service->getContainer($name);
+        } catch (BadResponseException $exception) {
+            if ($exception->getRequest()->getResponse()->getStatusCode() === 404) {
+                return false;
+            }
+            throw $exception;
+        }
     }
 }
--- tests/Unit/Document/DocumentTest.php
+++ tests/Unit/Document/DocumentTest.php
@@ -2,11 +2,13 @@
 
 namespace RoundPartner\Unit\Document;
 
+use OpenCloud\Tests\MockSubscriber;
+use OpenCloud\Tests\OpenCloudTestCase;
 use RoundPartner\Cloud\Service\Cloud;
 use RoundPartner\Cloud\Document\Document;
 use RoundPartner\Conf\Service;
 
-class DocumentTest extends \PHPUnit_Framework_TestCase
+class DocumentTest extends OpenCloudTestCase
 {
     /**
      * @var array
@@ -21,13 +23,32 @@ class DocumentTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $config = Service::get('opencloud');
-        $client = new Cloud($config['username'], $config['key']);
+        $this->client = new Cloud($config['username'], $config['key']);
         $this->config = Service::get('testclouddocument');
-        $this->service = new Document($client);
+        $this->service = new Document($this->client);
     }
 
     public function testCreateInstance()
     {
         $this->assertInstanceOf('RoundPartner\Cloud\Document\Document', $this->service);
+    }
+
+    public function testGetContainer()
+    {
+        $this->addMockSubscriber($this->makeResponse(null, 201));
+        $this->assertInstanceOf('OpenCloud\ObjectStore\Resource\Container', $this->service->getContainer($this->config['name']));
+    }
+
+    public function testGetContainerThrowsExceptionOnError()
+    {
+        $this->addMockSubscriber($this->makeResponse(null, 404));
+        $this->setExpectedException('OpenCloud\Common\Exceptions\InvalidArgumentError');
+        $this->service->getContainer('this/will/throw/an/exception');
+    }
+
+    public function testContainerExists()
+    {
+        $this->addMockSubscriber($this->makeResponse(null, 404));
+        $this->assertFalse($this->service->containerExists($this->config['name']));
     }
 }
