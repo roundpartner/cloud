59b50438b801fd2633b01d4efee7c7547271e473
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2017-08-26 18:08:32 +0100

    Throws exception if unserialised object has no class

--- src/Message/Message.php
+++ src/Message/Message.php
@@ -78,7 +78,7 @@ class Message
     private function unserialiseBody($body)
     {
         $object = unserialize($body);
-        if ($object instanceof __PHP_Incomplete_Class) {
+        if ('__PHP_Incomplete_Class' === get_class($object)) {
             throw new \Exception('Unable to unserialise message');
         }
         return $object;
--- tests/Unit/Message/MessageTest.php
+++ tests/Unit/Message/MessageTest.php
@@ -22,4 +22,40 @@ class MessageTest extends \PHPUnit_Framework_TestCase
         $this->setExpectedException('\RoundPartner\Cloud\Message\NoSignatureException', 'Message has no signature');
         $message->getBody();
     }
+
+    public function testUnserialiseBody()
+    {
+        $mock = new MessageMock();
+        $message = new Message($mock, 'no secret');
+        $serialised = 's:11:"Hello World";';
+        $result = $this->invokeMethod($message, 'unserialiseBody', [$serialised]);
+        $this->assertEquals('Hello World', $result);
+    }
+
+    public function testUnserialiseBodyExcepts()
+    {
+        $this->setExpectedException('\Exception', 'Unable to unserialise message');
+        $mock = new MessageMock();
+        $message = new Message($mock, 'no secret');
+        $serialised = 'O:35:"\RoundPartner\Cloud\Massage\Massage":1:{s:7:"massage";a:0:{}}';
+        $this->invokeMethod($message, 'unserialiseBody', [$serialised]);
+    }
+
+    /**
+     * Call protected/private method of a class.
+     *
+     * @param object &$object    Instantiated object that we will run method on.
+     * @param string $methodName Method name to call
+     * @param array  $parameters Array of parameters to pass into method.
+     *
+     * @return mixed Method return.
+     */
+    public function invokeMethod(&$object, $methodName, array $parameters = array())
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
 }
